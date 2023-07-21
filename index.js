bf5985a596868e0532e0ae64718e36ec3f7f886b
Olayanju Oluwadamilare Samuel <hadreytechsolutions@gmail.com>
2023-07-21 13:15:42 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+num1 = 2;
+num2 = 31;
+
+multiply = num1 * num2;
+
+console.log(multiply)
+
+
+
+num3 = 60;
+num4 = 56;
+
+mod = num3 % num4;
+
+console.log(mod);
+
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+
+
